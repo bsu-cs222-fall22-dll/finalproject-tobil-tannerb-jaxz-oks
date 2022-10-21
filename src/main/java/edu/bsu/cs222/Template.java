b932faaaee881bb8b3bf5f53ece05c9180628cbb
OK Schlatter <kenneth.schlatter@gmail.com>
2022-10-21 14:49:59 -0400

    Adds more learning tests for the ImgFlip API.
    One test uses boxes.
    The other test fetches the list of the most popular memes over the past 30 days.

--- src/main/java/edu/bsu/cs222/Template.java
+++ src/main/java/edu/bsu/cs222/Template.java
@@ -2,4 +2,6 @@ package edu.bsu.cs222;
 
 public class Template {
 
+    public Template(String memeTitle) {
+    }
 }
--- src/test/java/edu/bsu/cs222/LearningTest.java
+++ src/test/java/edu/bsu/cs222/LearningTest.java
@@ -1,14 +1,13 @@
 package edu.bsu.cs222;
 import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.NameValuePair;
-import org.apache.hc.core5.http.ParseException;
-import org.apache.hc.core5.http.ProtocolException;
+import org.apache.hc.core5.http.*;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.message.BasicNameValuePair;
 import org.junit.jupiter.api.Test;
@@ -47,11 +46,53 @@ public class LearningTest {
 
     }
 
+    @Test
+    public void makeMemeWithBoxes() throws IOException, ParseException {
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("https://api.imgflip.com/caption_image");
+
+        List<NameValuePair> parameters = new ArrayList<>();
+        parameters.add(new BasicNameValuePair("template_id", "181913649"));
+        parameters.add(new BasicNameValuePair("username", "edu.bsu.cs22.finalproject"));
+        parameters.add(new BasicNameValuePair("password", "rZxJQmKsSht7eZk"));
+        parameters.add(new BasicNameValuePair("boxes[0][text]", "making memes with mematic"));
+        parameters.add(new BasicNameValuePair("boxes[1][text]", "writing your own code to generate custom memes"));
+
+        httpPost.setEntity(new UrlEncodedFormEntity(parameters));
+
+        CloseableHttpResponse response = (CloseableHttpResponse) httpClient.execute(httpPost);
+        System.out.println(response.getCode() + "     " + response.getReasonPhrase());
+        HttpEntity entity = response.getEntity();
+        System.out.println(entity);
+        decode(entity);
+        EntityUtils.consume(entity);
+    }
+
     private void decode(HttpEntity encoded) throws IOException, ParseException {
         String jsonString = EntityUtils.toString(encoded);
         System.out.println(jsonString);
         HashMap<String, String> jsonMap = JsonPath.read(jsonString, "$.data");
         System.out.println(jsonMap.get("url"));
         System.out.println(jsonMap.get("page_url"));
+    }
+
+    @Test
+    public void getMemeTemplateList_test() throws IOException, ParseException {
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("https://api.imgflip.com/get_memes");
+
+        CloseableHttpResponse response = (CloseableHttpResponse) httpClient.execute(httpGet);
+        System.out.println(response.getCode() + "     " + response.getReasonPhrase());
+        HttpEntity entity = response.getEntity();
+
+        String jsonString = EntityUtils.toString(entity);
+        System.out.println(jsonString);
+
+        JSONArray jsonArray = JsonPath.read(jsonString, "$.data.memes");
+        HashMap jsonMap = (HashMap) jsonArray.get(0);
+        System.out.println(jsonMap);
+        System.out.println(jsonMap.get("url"));
+
+        EntityUtils.consume(entity);
     }
 }

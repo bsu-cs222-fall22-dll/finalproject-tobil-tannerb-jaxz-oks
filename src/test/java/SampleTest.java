3d3e15b994cbe8f6c62d9c8519fb27408fe8fd60
jazinkie <jazinkie@bsu.edu>
2022-10-21 11:49:42 -0400

    Make first test.

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,2 +1,14 @@
+import edu.bsu.cs222.Template;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SampleTest {
+
+    @Test
+    public void testCreateTemplate(){
+        String memeTitle = "Meme01";
+        Template template = new Template(memeTitle);
+        Assertions.assertTrue(true);
+    }
+
 }

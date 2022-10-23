d7c1a7d14fc89f963f264154eaa9b7cd513bf3d3
tobil <tobi.lott@bsu.edu>
2022-10-23 13:31:14 -0400

    DataFetcher Tests

--- src/main/java/edu/bsu/cs222/DataFetcher.java
+++ src/main/java/edu/bsu/cs222/DataFetcher.java
@@ -27,4 +27,6 @@ public class DataFetcher {
         return memeIndex;
     }
 
+
+
 }
--- src/test/java/edu/bsu/cs222/DataFetcherTest.java
+++ src/test/java/edu/bsu/cs222/DataFetcherTest.java
@@ -13,4 +13,25 @@ public class DataFetcherTest {
         String memeIndex = dataFetcher.getTemplateByName("Drake Hotline Bling");
         Assertions.assertEquals("0", memeIndex);
     }
+
+    @Test
+    public void nameToIndexTestTwo() throws IOException, ParseException {
+        DataFetcher dataFetcher = new DataFetcher();
+        String memeIndex = dataFetcher.getTemplateByName("One Does Not Simply");
+        Assertions.assertEquals("15", memeIndex);
+    }
+
+    @Test
+    public void nameToIndexWrongNameTest() throws IOException, ParseException {
+        DataFetcher dataFetcher = new DataFetcher();
+        String memeIndex = dataFetcher.getTemplateByName("drake hotline");
+        Assertions.assertEquals("Meme Does Not Exist", memeIndex);
+    }
+
+    @Test
+    public void nameToIndexNameThatDoesNotExist() throws IOException, ParseException {
+        DataFetcher dataFetcher = new DataFetcher();
+        String memeIndex = dataFetcher.getTemplateByName("CS222 Meme");
+        Assertions.assertEquals("Meme Does Not Exist", memeIndex);
+    }
 }

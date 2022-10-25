165761bc96453b20c2575e288651a9177f33f391
OK Schlatter <kenneth.schlatter@gmail.com>
2022-10-25 19:37:04 -0400

    Improve TemplateFormatter.java
    Now it returns a string, which is printed by UI.java and compared in TemplateTest.java.

--- src/main/java/edu/bsu/cs222/TemplateFormatter.java
+++ src/main/java/edu/bsu/cs222/TemplateFormatter.java
@@ -6,14 +6,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class TemplateFormatter {
-    public void formatTop20() throws IOException, ParseException {
+    public String formatTop20() throws IOException, ParseException {
         ArrayList<String> list = new  JSONParser().getMemeList();
         MemeAPIManager data = new MemeAPIManager();
+        StringBuilder top20 = new StringBuilder();
         for(int i = 0; i < list.size(); i++){
             String order = String.valueOf(i+1);
-            String name = data.getName(i);
+            String name = MemeAPIManager.getName(i);
             String url = data.getURL(i);
-            System.out.printf("(%s) Name: %-50s Example: %20s\n", order,name,url);
+            top20.append(String.format("(%s) Name: %-50s Example: %20s\n", order, name, url));
         }
+        return top20.toString();
     }
 }

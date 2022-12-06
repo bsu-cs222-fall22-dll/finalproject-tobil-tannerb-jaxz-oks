a747fffde95c7c45efee322a6c1db38ecefd69ca
tobil <tobi.lott@bsu.edu>
2022-12-05 20:10:32 -0500

    CustomizationTest updated to be an assertion

--- src/test/java/edu/bsu/cs222/CustomizationTest.java
+++ src/test/java/edu/bsu/cs222/CustomizationTest.java
@@ -1,9 +1,11 @@
 package edu.bsu.cs222;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class CustomizationTest {
+    //Only checks to make sure that a it is a url because the url changes everytime a new custom meme is created
     @Test
     public void customMemeTest() {
         List<Template> templateList = JSONParser.getTemplateList();
@@ -11,6 +13,11 @@ public class CustomizationTest {
         customization.addText("making memes with mematic");
         customization.addText("writing your own code to generate custom memes");
         String url = customization.getCustomMemeURL();
-        System.out.println(url);
+        StringBuilder test = null;
+        for(int i = 0; i < 5; i++){
+            char character = url.toCharArray()[i];
+            test.append(character);
+        }
+        Assertions.assertNotEquals(url, "http");
     }
 }
